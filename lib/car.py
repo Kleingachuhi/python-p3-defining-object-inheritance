4effe8f51a15c4d857e365a7308066063bb24a9b
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-12 14:47:57 +0300

    second_attempt

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+my_car = Car(36, 4)
+my_car.go()
+my_car.fill_up_tank()
+need_car_fuel = my_car.fill_up_tank()
+# print(need_car_fuel)
+car_is_on_the_go = my_car.go()
+# print(car_is_on_the_go)
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,15 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_number = wheel_number
+        self.wheel_size = wheel_size
+    def go(self):
+        return "vrrrrrrrooom!"
+    def fill_up_tank(self):
+        return "filling up!"
+my_wheel = Vehicle(48, 4)
+# print(my_wheel.wheel_number)
+# print(my_wheel.wheel_size)
+need_fuel = my_wheel.fill_up_tank()
+# print(need_fuel)
+need_to_go = my_wheel.go()
+# print(need_to_go)
